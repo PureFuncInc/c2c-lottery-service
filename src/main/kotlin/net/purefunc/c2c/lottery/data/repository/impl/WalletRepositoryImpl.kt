24f1f128a57961256d6af026068e69f4ef551537
Vincent Huang iMac <yfr.huang@hotmail.com>
2022-04-04 18:06:30 +0800

    * fix combination bug

--- src/main/kotlin/net/purefunc/c2c/lottery/data/repository/impl/WalletRepositoryImpl.kt
+++ src/main/kotlin/net/purefunc/c2c/lottery/data/repository/impl/WalletRepositoryImpl.kt
@@ -16,66 +16,50 @@ class WalletRepositoryImpl(
     private val walletTransactionDao: WalletTransactionDao,
 ) : WalletRepository {
 
-    override suspend fun payForOrder(email: String, orderUuid: String, amount: BigDecimal) =
-        run {
-            val wallet = walletDao.findByEmail(email) ?: throw IllegalStateException()
-            walletDao.updateBalanceByEmail(email, amount)
-            val tx = walletTransactionDao.save(
-                WalletTransactionDo(
-                    id = null,
-                    uuid = randomUUID(),
-                    orderUuid = orderUuid,
-                    walletId = wallet.id!!,
-                    type = TransactionType.ORDER,
-                    beforeBalance = wallet.balance,
-                    amount = amount,
-                    afterBalance = wallet.balance.minus(amount),
-                    createDate = genUnixMilli()
-                )
-            )
+    override suspend fun payForOrder(
+        email: String,
+        orderUuid: String,
+        amount: BigDecimal,
+    ) = operate(TransactionType.ORDER, email, orderUuid, amount)
 
-            tx.uuid
+    override suspend fun deposit(
+        email: String,
+        amount: BigDecimal,
+    ) = operate(TransactionType.DEPOSIT, email, "", amount)
+
+    override suspend fun withdraw(
+        email: String,
+        amount: BigDecimal,
+    ) = operate(TransactionType.WITHDRAW, email, "", amount)
+
+    suspend fun operate(
+        transactionType: TransactionType,
+        email: String,
+        orderUuid: String,
+        amount: BigDecimal,
+    ) = run {
+        val opAmount = when (transactionType) {
+            TransactionType.ORDER -> amount.negate()
+            TransactionType.DEPOSIT -> amount
+            TransactionType.WITHDRAW -> amount.negate()
         }
 
-    override suspend fun deposit(email: String, amount: BigDecimal) =
-        run {
-            val wallet = walletDao.findByEmail(email) ?: throw IllegalStateException()
-            walletDao.updateBalanceByEmail(email, amount)
-            val tx = walletTransactionDao.save(
-                WalletTransactionDo(
-                    id = null,
-                    uuid = randomUUID(),
-                    orderUuid = "",
-                    walletId = wallet.id!!,
-                    type = TransactionType.DEPOSIT,
-                    beforeBalance = wallet.balance,
-                    amount = amount,
-                    afterBalance = wallet.balance.plus(amount),
-                    createDate = genUnixMilli()
-                )
+        val wallet = walletDao.findByEmail(email) ?: throw IllegalStateException()
+        walletDao.updateBalanceByEmail(email, opAmount)
+        val tx = walletTransactionDao.save(
+            WalletTransactionDo(
+                id = null,
+                uuid = randomUUID(),
+                orderUuid = orderUuid,
+                walletId = wallet.id!!,
+                type = transactionType,
+                beforeBalance = wallet.balance,
+                amount = opAmount,
+                afterBalance = wallet.balance.plus(opAmount),
+                createDate = genUnixMilli()
             )
+        )
 
-            tx.uuid
-        }
-
-    override suspend fun withdraw(email: String, amount: BigDecimal) =
-        run {
-            val wallet = walletDao.findByEmail(email) ?: throw IllegalStateException()
-            walletDao.updateBalanceByEmail(email, amount.negate())
-            val tx = walletTransactionDao.save(
-                WalletTransactionDo(
-                    id = null,
-                    uuid = randomUUID(),
-                    orderUuid = "",
-                    walletId = wallet.id!!,
-                    type = TransactionType.WITHDRAW,
-                    beforeBalance = wallet.balance,
-                    amount = amount,
-                    afterBalance = wallet.balance.minus(amount),
-                    createDate = genUnixMilli()
-                )
-            )
-
-            tx.uuid
-        }
+        tx.uuid
+    }
 }
